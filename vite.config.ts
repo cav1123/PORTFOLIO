0ea5c14f37c9aa1cccfc647020338cd7b4b7d479
nino <ninomiya.takuya.80514@gmail.com>
2024-08-28 21:03:28 +0900

    vite.config 修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PORTFOLIO/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    outDir: 'dist'
-  },
-  base: './'
+  }
 })
